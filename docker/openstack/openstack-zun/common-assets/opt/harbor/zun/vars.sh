cdf18bc94e6862c89f795afed326e495c526ca99
intlabs <petebirley@gmail.com>
2016-10-24 00:41:48 +0200

    Zun WIP

--- docker/openstack/openstack-zun/common-assets/opt/harbor/zun/vars.sh
+++ docker/openstack/openstack-zun/common-assets/opt/harbor/zun/vars.sh
@@ -7,6 +7,13 @@
 : ${ZUN_DB_KEY:="/run/harbor/auth/user/tls.key"}
 : ${ZUN_DB_CERT:="/run/harbor/auth/user/tls.crt"}
 
+: ${ZUN_ETCD_SERVICE_PORT:="4004"}
+: ${ZUN_ETCD_PEER_PORT:="7004"}
+: ${ZUN_ETCD_SERVICE_HOST_SVC:="${ZUN_API_SERVICE_HOSTNAME}-etcd.${ZUN_SERVICE_NAMESPACE}.svc.$OS_DOMAIN"}
+: ${ZUN_ETCD_CA:="${ZUN_DB_CA}"}
+: ${ZUN_ETCD_KEY:="${ZUN_ETCD_KEY}"}
+: ${ZUN_ETCD_CERT:="${ZUN_ETCD_CERT}"}
+
 : ${ZUN_API_SVC_PORT:="9517"}
 : ${ZUN_API_TLS_CA:="/run/harbor/auth/ssl/tls.ca"}
 : ${ZUN_API_TLS_KEY:="/run/harbor/auth/ssl/tls.key"}
